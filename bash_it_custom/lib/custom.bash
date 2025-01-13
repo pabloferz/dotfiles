53303cced802566691c237d7ced47cbe0c3f1fe9
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2025-01-13 09:03:35 -0600

    Prepend instead of appeding local bin dirs

--- bash_it_custom/lib/custom.bash
+++ bash_it_custom/lib/custom.bash
@@ -1,5 +1,5 @@
 # Set PATH so it includes user's private bin if it exists
-pathmunge "$HOME/bin" after
+pathmunge "$HOME/bin"
 
 # Set PATH so it includes user's ~/.local/bin if it exists
-pathmunge "$HOME/.local/bin" after
+pathmunge "$HOME/.local/bin"
