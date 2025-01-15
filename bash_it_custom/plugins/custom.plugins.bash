c105a9c649c25798100f41005138909eeccc3cad
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2025-01-15 04:54:04 -0600

    Add micromamba setup

--- bash_it_custom/plugins/custom.plugins.bash
+++ bash_it_custom/plugins/custom.plugins.bash
@@ -1,3 +1,7 @@
+if command -v micromamba 2>&1 >/dev/null; then
+    eval "$(micromamba shell hook --shell bash)"
+fi
+
 if command -v pixi 2>&1 >/dev/null; then
     eval "$(pixi completion --shell bash)"
 fi
